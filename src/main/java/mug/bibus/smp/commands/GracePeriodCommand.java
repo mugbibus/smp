ced5ea59f3eadb2260e5195ece542603ea607a13
athinaslime <athinaslime@gmail.com>
2024-12-14 13:30:58 +0200

    fix: fixed opposite grace period message

--- src/main/java/mug/bibus/smp/commands/GracePeriodCommand.java
+++ src/main/java/mug/bibus/smp/commands/GracePeriodCommand.java
@@ -19,7 +19,7 @@ public class GracePeriodCommand {
     public void executeGracePeriod() {
         combatConfiguration.setGracePeriod(!combatConfiguration.isGracePeriod());
         combatConfiguration.saveConfiguration();
-        Bukkit.broadcast(combatConfiguration.isGracePeriod()
+        Bukkit.broadcast(!combatConfiguration.isGracePeriod()
                 ? Component.text("Grace Period has been disabled. You are now able to hit other players").color(CC.PRIMARY)
                 : CC.GRACE_PERIOD_MESSAGE);
     }
